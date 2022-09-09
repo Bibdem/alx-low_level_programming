9d8bdf8ef3ed3a8369e247aa81acfd751acb619e
bibdem <bernardidem4@gmail.com>
2022-09-08 18:17:53 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - Entry point
- * Description: prints "\" Programming is like building a multilingual puzzle"
+ * main - outputs the required statement.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
