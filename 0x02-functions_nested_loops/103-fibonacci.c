3b350e915880c55a534eb3c51d030f22bd255435
bibdem <bernardidem4@gmail.com>
2022-09-14 14:40:26 -0700

    task15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,7 +5,7 @@
  * fibonacci suit numbers
  * Return: return 0
  */
-int mian(void)
+int main(void)
 {
 unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
 while (n3 <= 4000000)
@@ -15,7 +15,7 @@ n1 = n2;
 n2 = n3;
 
 if ((n1 % 2) == 0)
-sum += n1;
+sum += n1:
 
 }
 printf("%ld\n", sum);
